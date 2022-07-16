e4e54f6869f163b5d0b82a24454cf6bde692d770
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-07-17 00:53:38 +0200

    jdbc

--- src/test/java/jdbctests/TestConnection.java
+++ src/test/java/jdbctests/TestConnection.java
@@ -11,7 +11,7 @@ public class TestConnection {
         Connection connection = DriverManager.getConnection(dbUrl,dbUsername, dbPassword);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM regions ");
-
+/*
         //once you set up connection default pointer looks for 0
         //next() --> move pointer to first row
         resultSet.next();
@@ -21,6 +21,27 @@ public class TestConnection {
         //getting information with column index(starts 1)
         System.out.println(resultSet.getString(2));
         System.out.println(resultSet.getInt(1));
+
+        // 1 - Europe
+        // 2 - Americas
+
+        System.out.println(resultSet.getInt(1)+ " - "+ resultSet.getString(2));
+
+        //move to second row
+        resultSet.next();
+        System.out.println(resultSet.getInt(1)+ " - "+ resultSet.getString(2));
+
+        //move to third row
+        resultSet.next();
+        System.out.println(resultSet.getInt(1)+ " - "+ resultSet.getString(2));
+
+
+ */
+        while(resultSet.next()){
+
+            System.out.println(resultSet.getInt(1)+ " - "+ resultSet.getString(2));
+
+        }
 
         //close connection
         resultSet.close();
